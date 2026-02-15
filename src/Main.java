f840a0fef48fb882b6976f7e15a612e1846d53b0
Mikenin-Ev <ormembar7@gmail.com>
2026-02-16 01:51:06 +0300

    Домашка_10

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача № 1.");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println(" ");
+        System.out.println("Задача №2.");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println(" ");
+        System.out.println("Задача №3.");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace('ё', 'е'));
+    }
+}
